5b75596525d06826689337168068d9c68e05c923
SanjayDutta <sanjay.02.aug.1998@gmail.com>
2020-11-22 20:33:03 +0530

    Removed unecessary items

--- app.js
+++ app.js
@@ -19,11 +19,15 @@ app.use("/",trialHomePage);*/
 
 //console.log("Called");
 app.get("/", require("./routes/trialHomePage"));
+
 app.get("/movie", require("./routes/getInfo"));
 app.get("/tv", require("./routes/getInfo"));
+
 app.get("/search", require("./routes/searchMovie"));
-app.get("/home", require("./routes/homePage"));   //cache enabled
-app.get("/discover", require("./routes/discover"))  //cache enabled
+
+
+app.get("/home", require("./routes/home"));   //cache enabled //get all trending,entry-point to discover
+app.get("/home/discover", require("./routes/home"))  //cache enabled
 
 
 
--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const redis = require("redis");
-
-
-
 var router = express.Router();
-const checkContent = require("../models/checkValidity.js")
+const redis = require("redis");
 const getMovieOrTvDetails = require("../models/getMovieOrTvDetails.js");
+const checkContent = require("../models/checkValidity.js")
+
+
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 app.set('view engine', 'ejs');
-//const REDIS_PORT = process.env.PORT || 6379;
-
 
 //Middleware
 const client = redis.createClient({
@@ -22,8 +21,9 @@ const client = redis.createClient({
     host: process.env.REDIS_HOST,
     password: process.env.REDIS_PASSWORD
 });
-function cache(req, res, next) {
-    client.get("discoverTVorMovieString", (err, data) => {
+
+function cacheForTrending(req, res, next) {
+    client.get("trendingContentString", (err, data) => {
         if (err) throw err;
         if (data != null) {
             res.send(data);
@@ -34,8 +34,31 @@ function cache(req, res, next) {
     })
 }
 
+function cacheForDiscover(req, res, next) {
+    client.get("discoverTVorMovieString", (err, data) => {
+        if (err) throw err;
+        if (data != null) {
+            res.send(JSON.parse(data));
+        }
+        else {
+            next();
+        }
+    })
+}
 
-router.get("/discover", cache, async function (req, res) {
+
+
+router.get("/home", cacheForTrending, async function (req, res) {
+
+    let trendingContent = await getMovieOrTvDetails.getDetails("week", "trending/all");
+    let trendingContentString = JSON.stringify(trendingContent);
+    client.setex("trendingContentString", 3600, trendingContentString);
+    res.send(trendingContent);
+
+    //also create a banner to encourage user to discover
+});
+
+router.get("/home/discover", cacheForDiscover, async function (req, res) {
 
     let sort_by = "&sort_by=" + (req.query.sort_by || "popularity.desc");
     let page = "&page=" + (req.query.page || "1");
@@ -63,8 +86,3 @@ router.get("/discover", cache, async function (req, res) {
 });
 
 module.exports = router;
-
-/*client.flushdb( function (err, succeeded) {
-    console.log(succeeded); // will be true if successfull
-});
-*/
